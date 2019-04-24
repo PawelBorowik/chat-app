b2f0de35bcbb2622d3491227b38b36baea82e3d0
PAWEŁEK\pawel_000 <pawelbor1@op.pl>
2019-04-24 21:49:41 +0200

    Add handleInputAddText

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state={
+
+  }
+  handleInputAddText=(e)=>{
+    console.log(e.target.value)
+
+  }
+
   render() {
     return (
       <div className="App">
-      <input type="text"/>
+      <input onChange={this.handleInputAddText} type="text"/>
       <button>reset</button>
       <p className="text"></p>
        
